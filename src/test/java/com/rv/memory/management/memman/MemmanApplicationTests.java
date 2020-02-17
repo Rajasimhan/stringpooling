3a880b66a2c33cc63392d25807f6608ca4560f43
rajasimhan varadharajan <rajasimhan.varadharajan@gmail.com>
2020-02-17 11:16:16 -0500

    initial commit

---
+++ src/test/java/com/rv/memory/management/memman/MemmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rv.memory.management.memman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
